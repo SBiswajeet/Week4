93313496520a2f7a8cce94bfe43d8ded3d5e6373
SBiswajeet <sbiswajeet2345@gmail.com>
2021-12-13 18:22:28 +0530

    first commit

---
+++ src/main/java/com/lti/Producer/entity/User.java
@@ -0,0 +1,98 @@
+package com.lti.Producer.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name="Users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="age")
+	private int age;
+	
+	@Column(name="address")
+	private String address;
+	
+	@Column(name="type_of_account")
+	private String typeOfAccount;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getTypeOfAccount() {
+		return typeOfAccount;
+	}
+
+	public void setTypeOfAccount(String typeOfAccount) {
+		this.typeOfAccount = typeOfAccount;
+	}
+
+	public User(long id, String firstName, String lastName, int age, String address, String typeOfAccount) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.address = address;
+		this.typeOfAccount = typeOfAccount;
+	}
+
+	public User() {
+		
+	}
+
+
+}
---
+++ src/main/java/com/lti/Producer/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.lti.Producer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lti.Producer.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
